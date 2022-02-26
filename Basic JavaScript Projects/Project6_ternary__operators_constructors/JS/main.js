ce7207a0fc48c1ef84de363aa7dc438036da6458
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-02-26 16:25:42 -0500

    fixed typos

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -34,7 +34,7 @@ function count_Function(){
     document.getElementById("Nested_Function").innerHTML = Count();
     function Count(){
         var Starting_point = 9;
-        function Plus_one() {Starting_point += 1;}
+        function Plus_one() {Starting_point += 1}
         Plus_one();
         return Starting_point;
     }
